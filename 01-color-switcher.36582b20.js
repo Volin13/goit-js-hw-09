44a49beda78a9c5983b3b0661910379ca835e6a2
Volin13 <110535249+Volin13@users.noreply.github.com>
2022-11-14 18:55:03 +0000

    Deploying to gh-pages from @ Volin13/goit-js-hw-09@c1d95888e53278e4365c4c6a1ba1ad8f164ba283 🚀

---
+++ 01-color-switcher.36582b20.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let o=null;function d(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.setAttribute("disabled",!0)}t.addEventListener("click",(function(){o=setInterval(d,1e3),t.toggleAttribute("disabled"),e.removeAttribute("disabled",!0)})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled"),e.toggleAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.36582b20.js.map
---
+++ 01-color-switcher.89b5f23a.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=null;function n(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),t.setAttribute("disabled",!0)}t.addEventListener("click",(function(){o=setInterval(n,1e3),t.toggleAttribute("disabled"),e.removeAttribute("disabled",!0)})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled"),e.toggleAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.89b5f23a.js.map
